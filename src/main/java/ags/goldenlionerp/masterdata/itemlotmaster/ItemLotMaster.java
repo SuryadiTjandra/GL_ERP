2ac959dc4a93dbb5c605f86bd90f8a47d6609afd
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-06-25 17:32:27 +0700

    Start working on lot master

---
+++ src/main/java/ags/goldenlionerp/masterdata/itemlotmaster/ItemLotMaster.java
@@ -0,0 +1,195 @@
+package ags.goldenlionerp.masterdata.itemlotmaster;
+
+import java.sql.Timestamp;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+
+import ags.goldenlionerp.entities.DatabaseEntity;
+import ags.goldenlionerp.masterdata.businessunit.BusinessUnit;
+import ags.goldenlionerp.masterdata.itemmaster.ItemMaster;
+
+@Entity
+@Table(name="T4108")
+@AttributeOverrides({
+	@AttributeOverride(name="inputUserId", column=@Column(name="LTUID")),
+	@AttributeOverride(name="inputDate", column=@Column(name="LTDTIN")),
+	@AttributeOverride(name="inputTime", column=@Column(name="LTTMIN")),
+	@AttributeOverride(name="lastUpdateUserId", column=@Column(name="LTUIDM")),
+	@AttributeOverride(name="lastUpdateDate", column=@Column(name="LTDTLU")),
+	@AttributeOverride(name="lastUpdateTime", column=@Column(name="LTTMLU")),
+	@AttributeOverride(name="computerId", column=@Column(name="LTCID"))
+})
+public class ItemLotMaster extends DatabaseEntity {
+	
+	@EmbeddedId
+	@JsonUnwrapped
+	private ItemLotMasterPK pk;
+	
+	@Column(name="LTLDSC")
+	private String lotDescription;
+	
+	@Column(name="LTLSC")
+	private String lotStatusCode;
+	
+	@Column(name="LTSERN")
+	private String serialNumber;
+	
+	@Column(name="LTMLOT1")
+	private String memoLot1;
+	
+	@Column(name="LTMLOT2")
+	private String memoLot2;
+	
+	@Column(name="LTMLOT3")
+	private String memoLot3;
+	
+	@Column(name="LTEXPDT")
+	private Timestamp expiredDate;
+	
+	@Column(name="LTBBDT")
+	private Timestamp bestBeforeDate;
+	
+	@Column(name="LTBODT")
+	private Timestamp baseOnDate;
+	
+	@Column(name="LTLEDT")
+	private Timestamp lotEffectiveDate;
+	
+	@Column(name="LTOHDT")
+	private Timestamp onHandDate;
+	
+	@Column(name="LTCLDT")
+	private Timestamp closedDate;
+	
+	@JoinColumn(name="LTBUID", updatable = false, insertable = false)
+	@ManyToOne(fetch=FetchType.LAZY, optional=false)
+	private BusinessUnit businessUnit;
+	
+	@JoinColumn(name="LTINUM", updatable = false, insertable = false)
+	@ManyToOne(fetch=FetchType.LAZY, optional=false)
+	private ItemMaster item;
+
+	public ItemLotMasterPK getPk() {
+		return pk;
+	}
+
+	public String getLotDescription() {
+		return lotDescription;
+	}
+
+	public String getLotStatusCode() {
+		return lotStatusCode;
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public String getMemoLot1() {
+		return memoLot1;
+	}
+
+	public String getMemoLot2() {
+		return memoLot2;
+	}
+
+	public String getMemoLot3() {
+		return memoLot3;
+	}
+
+	public Timestamp getExpiredDate() {
+		return expiredDate;
+	}
+
+	public Timestamp getBestBeforeDate() {
+		return bestBeforeDate;
+	}
+
+	public Timestamp getBaseOnDate() {
+		return baseOnDate;
+	}
+
+	public Timestamp getLotEffectiveDate() {
+		return lotEffectiveDate;
+	}
+
+	public Timestamp getOnHandDate() {
+		return onHandDate;
+	}
+
+	public Timestamp getClosedDate() {
+		return closedDate;
+	}
+
+	public BusinessUnit getBusinessUnit() {
+		return businessUnit;
+	}
+
+	public ItemMaster getItem() {
+		return item;
+	}
+
+	void setPk(ItemLotMasterPK pk) {
+		this.pk = pk;
+	}
+
+	void setLotDescription(String lotDescription) {
+		this.lotDescription = lotDescription;
+	}
+
+	void setLotStatusCode(String lotStatusCode) {
+		this.lotStatusCode = lotStatusCode;
+	}
+
+	void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	void setMemoLot1(String memoLot1) {
+		this.memoLot1 = memoLot1;
+	}
+
+	void setMemoLot2(String memoLot2) {
+		this.memoLot2 = memoLot2;
+	}
+
+	void setMemoLot3(String memoLot3) {
+		this.memoLot3 = memoLot3;
+	}
+
+	void setExpiredDate(Timestamp expiredDate) {
+		this.expiredDate = expiredDate;
+	}
+
+	void setBestBeforeDate(Timestamp bestBeforeDate) {
+		this.bestBeforeDate = bestBeforeDate;
+	}
+
+	void setBaseOnDate(Timestamp baseOnDate) {
+		this.baseOnDate = baseOnDate;
+	}
+
+	void setLotEffectiveDate(Timestamp lotEffectiveDate) {
+		this.lotEffectiveDate = lotEffectiveDate;
+	}
+
+	void setOnHandDate(Timestamp onHandDate) {
+		this.onHandDate = onHandDate;
+	}
+
+	void setClosedDate(Timestamp closedDate) {
+		this.closedDate = closedDate;
+	}
+	
+	
+}
