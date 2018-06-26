ac80979479e9ac66ffc317741605fc132a750c38
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-06-26 15:28:06 +0700

    Finished lot master

--- src/test/java/ags/goldenlionerp/apiTests/LotMasterApiTest.java
+++ src/test/java/ags/goldenlionerp/apiTests/LotMasterApiTest.java
@@ -7,9 +7,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
@@ -17,42 +21,39 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.jayway.jsonpath.JsonPath;
 
-import ags.goldenlionerp.masterdata.itembranchinfo.ItemBranchInfoPK;
+import ags.goldenlionerp.masterdata.lotmaster.LotMasterPK;
 import ags.goldenlionerp.util.WebIdUtil;
 
-public class ItemBranchInfoApiTest extends ApiTestBase<ItemBranchInfoPK> {
-
+public class LotMasterApiTest extends ApiTestBase<LotMasterPK> {
 
 	@Override
 	Map<String, Object> requestObject() throws Exception {
 		Map<String, Object> map = new HashMap<>();
-		map.put("branchCode", newId().getBranchCode());
-		map.put("itemCode", newId().getItemCode());
-		map.put("stockingType", "C");
-		map.put("reorderQty", "100.15");
-		map.put("serialNumberRequired", true);
-		
-		Map<String, Object> mapPar = new HashMap<>();
-		mapPar.put("profitMarginMinimumFactor", 25.75);
-		mapPar.put("rankByInventoryInvestment", "C");
-		map.put("parameters", mapPar);
+		map.put("businessUnitId", newId().getBusinessUnitId());
+		map.put("itemCode", newId.getItemCode());
+		map.put("serialLotNo", newId.getSerialLotNo());
+		map.put("lotDescription", "Test New Lot");
+		map.put("lotStatusCode", "A");
+		map.put("expiredDate", LocalDateTime.of(2019, Month.FEBRUARY, 14, 13, 5, 0));
+		map.put("lotEffectiveDate", LocalDateTime.of(2018, Month.JULY, 15, 13, 5, 0));
 		return map;
 	}
 
 	@Override
 	String baseUrl() {
-		return "/api/itembranches/";
+		return "/api/lots/";
 	}
 
 	@Override
-	ItemBranchInfoPK existingId() {
-		return new ItemBranchInfoPK("110", "ACC.BATERAI - CMOS\t\t\t");
+	LotMasterPK existingId() {
+		return new LotMasterPK("123", "TEST.TEST", "test");
 	}
 
 	@Override
-	ItemBranchInfoPK newId() {
-		return new ItemBranchInfoPK("123", "TEST.TEST");
+	LotMasterPK newId() {
+		return new LotMasterPK("100", "ACC.CD-CDBLANK", "testNew");
 	}
+
 	
 	@Override @Test
 	public void getTestSingle() throws Exception {
@@ -61,30 +62,24 @@ public class ItemBranchInfoApiTest extends ApiTestBase<ItemBranchInfoPK> {
 		mockMvc.perform(get(baseUrl + urlId).accept(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.businessUnitId").value(existingId.getBusinessUnitId()))
 				.andExpect(jsonPath("$.itemCode").value(existingId.getItemCode()))
-				.andExpect(jsonPath("$.glClass").value("INFG"))
-				.andExpect(jsonPath("$.reorderQty").value(0))
-				.andExpect(jsonPath("$.inventoryLotCreation").value(false))
-				.andExpect(jsonPath("$.lotStatusCode").value(""))
-				.andExpect(jsonPath("$.parameters.discountFactor").value(0))
-				.andExpect(jsonPath("$.dataGroups.brandCode").value(""))
-				.andExpect(jsonPath("$.dataGroups.salesReportingCode5").value(""));
-
+				.andExpect(jsonPath("$.serialLotNo").value(existingId.getSerialLotNo()))
+				.andExpect(jsonPath("$.lotDescription").value("Test lot"))
+				.andExpect(jsonPath("$.lotStatusCode").value("H"))
+				.andExpect(jsonPath("$.serialNumber").value("test"))
+				.andExpect(jsonPath("$.expiredDate").value(LocalDateTime.of(2019, Month.JUNE, 29, 0, 0, 0).format(DateTimeFormatter.ISO_DATE_TIME)))
+				.andExpect(jsonPath("$.lotEffectiveDate").value(LocalDateTime.of(2018, Month.JUNE, 26, 0, 0).format(DateTimeFormatter.ISO_DATE_TIME)))
+				.andExpect(jsonPath("$.closedDate").value(LocalDateTime.of(2019, Month.JUNE, 30, 0, 0).format(DateTimeFormatter.ISO_DATE_TIME)));
 	}
 
-	@Override
-	@Test
+	@Override @Test
 	public void getTestCollection() throws Exception {
 		mockMvc.perform(get(baseUrl).accept(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$._embedded.itembranches[1].branchCode").value("110"))
-				.andExpect(jsonPath("$._embedded.itembranches.length()").value(20))
-				.andExpect(jsonPath("$.page.totalElements").value(3008));
-
+				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override @Test @Rollback
 	public void createTestWithPost() throws Exception {
 		assumeNotExists(baseUrl + WebIdUtil.toWebId(newId.toString()));
@@ -98,19 +93,17 @@ public class ItemBranchInfoApiTest extends ApiTestBase<ItemBranchInfoPK> {
 		
 		em.flush(); em.clear();
 		
-		Map<String, Object> mapPar = (Map<String, Object>) requestObject.get("parameters");
-
-		String getRes = mockMvc.perform(get(baseUrl + newId))
+		String getRes = mockMvc.perform(get(baseUrl + WebIdUtil.toWebId(newId.toString())))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(jsonPath("$.itemCode").value(newId.getItemCode()))
-				.andExpect(jsonPath("$.branchCode").value(newId.getBranchCode()))
-				.andExpect(jsonPath("$.stockingType").value(requestObject.get("stockingType")))
-				.andExpect(jsonPath("$.reorderQty").value(requestObject.get("reorderQty")))
-				.andExpect(jsonPath("$.serialNumberRequired").value(requestObject.get("serialNumberRequired")))
-				.andExpect(jsonPath("$.minimumReorderQty").value(0))
-				.andExpect(jsonPath("$.parameters.profitMarginMinimumFactor").value(mapPar.get("profitMarginMinimumFactor")))
-				.andExpect(jsonPath("$.dataGroups.brandCode").value(""))
-				.andExpect(jsonPath("$.parameters.discountFactor").value(0))
+				.andExpect(jsonPath("$.businessUnitId").value(newId.getBusinessUnitId()))
+				.andExpect(jsonPath("$.serialLotNo").value(newId.getSerialLotNo()))
+				.andExpect(jsonPath("$.lotDescription").value(requestObject.get("lotDescription")))
+				.andExpect(jsonPath("$.lotStatusCode").value(requestObject.get("lotStatusCode")))
+				.andExpect(jsonPath("$.expiredDate").value(((LocalDateTime)requestObject.get("expiredDate")).format(DateTimeFormatter.ISO_DATE_TIME)))
+				.andExpect(jsonPath("$.lotEffectiveDate").value(((LocalDateTime)requestObject.get("lotEffectiveDate")).format(DateTimeFormatter.ISO_DATE_TIME)))
+				.andExpect(jsonPath("$.closedDate").value(Matchers.nullValue()))
+				.andExpect(jsonPath("$.memoLot1").value(""))
 				.andReturn().getResponse().getContentAsString();
 				
 		assertCreationInfo(getRes);
@@ -129,17 +122,17 @@ public class ItemBranchInfoApiTest extends ApiTestBase<ItemBranchInfoPK> {
 
 	@Override @Test @Rollback
 	public void deleteTest() throws Exception {
-		assumeExists(baseUrl + WebIdUtil.toWebId(existingId.toString()));
+		String url = baseUrl + WebIdUtil.toWebId(existingId.toString());
+		assumeExists(url);
 		
-		mockMvc.perform(delete(baseUrl + existingId))
+		mockMvc.perform(delete(url))
 			.andExpect(MockMvcResultMatchers.status().isNoContent());
 	
-		mockMvc.perform(get(baseUrl + existingId))
+		mockMvc.perform(get(url))
 			.andExpect(MockMvcResultMatchers.status().isNotFound());
 
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override @Test @Rollback
 	public void updateTestWithPatch() throws Exception {
 		assumeExists(baseUrl + existingId);
@@ -147,10 +140,7 @@ public class ItemBranchInfoApiTest extends ApiTestBase<ItemBranchInfoPK> {
 		String beforePatch = mockMvc.perform(get(baseUrl + existingId))
 								.andReturn().getResponse().getContentAsString();
 		
-		requestObject.remove("stockingType");
-		
-		Map<String, Object> mapPar = (Map<String, Object>) requestObject.get("parameters");
-		
+		requestObject.remove("lotEffectiveDate");
 		
 		mockMvc.perform(patch(baseUrl + existingId)
 						.accept(MediaType.APPLICATION_JSON)
@@ -158,20 +148,18 @@ public class ItemBranchInfoApiTest extends ApiTestBase<ItemBranchInfoPK> {
 						.content(mapper.writeValueAsString(requestObject)))
 				.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
 	
-		em.flush();
-		em.clear();
+		em.flush();em.clear();
 		
 		String getResult = mockMvc.perform(get(baseUrl + existingId))
-							.andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-							.andExpect(jsonPath("$.itemCode").value(existingId.getItemCode()))
-							.andExpect(jsonPath("$.branchCode").value(existingId.getBranchCode()))
-							.andExpect(jsonPath("$.stockingType").value((String) JsonPath.read(beforePatch, "$.stockingType")))
-							.andExpect(jsonPath("$.reorderQty").value(requestObject.get("reorderQty")))
-							.andExpect(jsonPath("$.serialNumberRequired").value(requestObject.get("serialNumberRequired")))
-							.andExpect(jsonPath("$.minimumReorderQty").value((Double) JsonPath.read(beforePatch, "$.minimumReorderQty")))
-							.andExpect(jsonPath("$.parameters.profitMarginMinimumFactor").value(mapPar.get("profitMarginMinimumFactor")))
-							.andExpect(jsonPath("$.dataGroups.brandCode").value((String) JsonPath.read(beforePatch, "$.dataGroups.brandCode")))
-							.andExpect(jsonPath("$.parameters.discountFactor").value((Double) JsonPath.read(beforePatch, "$.parameters.discountFactor")))
+				.andExpect(jsonPath("$.itemCode").value(existingId.getItemCode()))
+				.andExpect(jsonPath("$.businessUnitId").value(existingId.getBusinessUnitId()))
+				.andExpect(jsonPath("$.serialLotNo").value(existingId.getSerialLotNo()))
+				.andExpect(jsonPath("$.lotDescription").value(requestObject.get("lotDescription")))
+				.andExpect(jsonPath("$.lotStatusCode").value(requestObject.get("lotStatusCode")))
+				.andExpect(jsonPath("$.expiredDate").value(((LocalDateTime)requestObject.get("expiredDate")).format(DateTimeFormatter.ISO_DATE_TIME)))
+				.andExpect(jsonPath("$.lotEffectiveDate").value((String)JsonPath.read(beforePatch, "$.lotEffectiveDate")))
+				.andExpect(jsonPath("$.closedDate").value((String)JsonPath.read(beforePatch, "$.closedDate")))
+				.andExpect(jsonPath("$.memoLot1").value((String)JsonPath.read(beforePatch, "$.memoLot1")))
 							.andReturn().getResponse().getContentAsString();
 		assertUpdateInfo(getResult, beforePatch);
 
@@ -179,11 +167,13 @@ public class ItemBranchInfoApiTest extends ApiTestBase<ItemBranchInfoPK> {
 
 	@Override @Test @Rollback
 	public void updateTestWithPut() throws Exception {
+		assumeExists(baseUrl+existingId);
+		
 		mockMvc.perform(put(baseUrl + existingId)
 						.content(mapper.writeValueAsString(requestObject)))
 				.andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
 
 	}
 
-
+	
 }
