1398bbf8b415e80099eb0e556bd0a88205a0f896
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-07-05 15:28:36 +0700

    AutomaticAccountingInstruction

---
+++ src/main/java/ags/goldenlionerp/system/aai/AutomaticAccountingInstructionRepository.java
@@ -0,0 +1,10 @@
+package ags.goldenlionerp.system.aai;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="aai")
+public interface AutomaticAccountingInstructionRepository
+		extends PagingAndSortingRepository<AutomaticAccountingInstruction, AutomaticAccountingInstructionPK> {
+
+}
