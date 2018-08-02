895c489bc92c250310b2e7b5b44e1757873e4c54
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-08-02 12:04:18 +0700

    First successful test under new testing framework

--- src/test/java/ags/goldenlionerp/apiTests/NextNumberConstantApiTest.java
+++ src/test/java/ags/goldenlionerp/apiTests/NextNumberConstantApiTest.java
@@ -1,27 +1,19 @@
 package ags.goldenlionerp.apiTests;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
-import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
+import org.springframework.test.web.servlet.ResultActions;
 import com.jayway.jsonpath.JsonPath;
 
-public class NextNumberConstantApiTest extends ApiTestBase<String> {
+public class NextNumberConstantApiTest extends ApiTestBaseNew<String> {
 
 	@Override
-	Map<String, Object> requestObject() throws Exception {
+	protected Map<String, Object> requestObject() throws Exception {
 		Map<String, Object> map = new HashMap<>();
 		map.put("documentType", newId());
 		map.put("resetNumber", 2);
@@ -32,128 +24,72 @@ public class NextNumberConstantApiTest extends ApiTestBase<String> {
 	}
 
 	@Override
-	String baseUrl() {
+	protected String baseUrl() {
 		return "/api/nextNumberConstants/";
 	}
 
 	@Override
-	String existingId() {
+	protected String existingId() {
 		return "FD";
 	}
 
 	@Override
-	String newId() {
+	protected String newId() {
 		return "FM";
-	}
-	
-	@Override @Test
-	public void getTestSingle() throws Exception {
-		
-		mockMvc.perform(get(baseUrl + existingId).accept(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$.documentType").value(existingId))
-				.andExpect(jsonPath("$.resetNumber").value(0))
-				.andExpect(jsonPath("$.resetMethod").value("CY"))
-				.andExpect(jsonPath("$.includeMonthInNextNumber").value(true))
-				.andExpect(jsonPath("$.includeYearInNextNumber").value(true));
-
-	}
-
-	@Override @Test
-	public void getTestCollection() throws Exception {
-		mockMvc.perform(get(baseUrl).accept(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$._embedded.nextNumberConstants").exists());
 	}
 
 	@Override @Test @Rollback
 	public void createTestWithPost() throws Exception {
-		mockMvc.perform(delete(baseUrl + newId));		
-		assumeNotExists(baseUrl+newId);
-		
-		mockMvc.perform(post(baseUrl)
-						.accept(MediaType.APPLICATION_JSON)
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(mapper.writeValueAsString(requestObject)))
-				.andDo(MockMvcResultHandlers.print())
-				.andExpect(MockMvcResultMatchers.status().isCreated());
-		
-		em.flush(); em.clear();
-		
-		String getRes = mockMvc.perform(get(baseUrl + newId))
-				.andExpect(jsonPath("$.documentType").value(requestObject.get("documentType")))
-				.andExpect(jsonPath("$.resetNumber").value(requestObject.get("resetNumber")))
-				.andExpect(jsonPath("$.resetMethod").value(requestObject.get("resetMethod")))
-				.andExpect(jsonPath("$.includeMonthInNextNumber").value(requestObject.get("includeMonthInNextNumber")))
-				.andExpect(jsonPath("$.includeYearInNextNumber").value(requestObject.get("includeYearInNextNumber")))
-				.andReturn().getResponse().getContentAsString();
-				
-		assertCreationInfo(getRes);
-
+		performer.performDelete(baseUrl() + newId());
+		super.createTestWithPost();
 	}
-
+	
 	@Override @Test @Rollback
 	public void createTestWithPut() throws Exception {
-		mockMvc.perform(delete(baseUrl + newId));		
-		assumeNotExists(baseUrl+newId);
-		
-		mockMvc.perform(put(baseUrl + newId)
-						.content(mapper.writeValueAsString(requestObject)))
-				.andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
-
-	}
-
-	@Override @Test @Rollback
-	public void deleteTest() throws Exception {
-		assumeExists(baseUrl + existingId);
-		
-		mockMvc.perform(delete(baseUrl + existingId))
-			.andExpect(MockMvcResultMatchers.status().isNoContent());
-	
-		mockMvc.perform(get(baseUrl + existingId))
-			.andExpect(MockMvcResultMatchers.status().isNotFound());
-
+		performer.performDelete(baseUrl() + newId());
+		super.createTestWithPut();
 	}
 
 	@Override @Test @Rollback
 	public void updateTestWithPatch() throws Exception {
-		assumeExists(baseUrl + existingId);
-		
-		String beforePatch = mockMvc.perform(get(baseUrl + existingId))
-								.andReturn().getResponse().getContentAsString();
-		
 		requestObject.remove("resetNumber");
-		
-		mockMvc.perform(patch(baseUrl + existingId)
-						.accept(MediaType.APPLICATION_JSON)
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(mapper.writeValueAsString(requestObject)))
-					.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-	
-		em.flush();	em.clear();
-		
-		String getResult = mockMvc.perform(get(baseUrl + existingId))
-				.andExpect(jsonPath("$.documentType").value(existingId))
-				.andExpect(jsonPath("$.resetNumber").value((Integer) JsonPath.read(beforePatch, "$.resetNumber")))
-				.andExpect(jsonPath("$.resetMethod").value(requestObject.get("resetMethod")))
-				.andExpect(jsonPath("$.includeMonthInNextNumber").value(requestObject.get("includeMonthInNextNumber")))
-				.andExpect(jsonPath("$.includeYearInNextNumber").value(requestObject.get("includeYearInNextNumber")))
-				.andReturn().getResponse().getContentAsString();
-		
-		assertUpdateInfo(getResult, beforePatch);
-
+		super.updateTestWithPatch();
 	}
 
-	@Override @Test @Rollback
-	public void updateTestWithPut() throws Exception {		
-		assumeExists(baseUrl+existingId);
+	@Override
+	public void assertGetSingleResult(ResultActions action) throws Exception {
+		action
+		.andExpect(jsonPath("$.documentType").value(existingId))
+		.andExpect(jsonPath("$.resetNumber").value(0))
+		.andExpect(jsonPath("$.resetMethod").value("CY"))
+		.andExpect(jsonPath("$.includeMonthInNextNumber").value(true))
+		.andExpect(jsonPath("$.includeYearInNextNumber").value(true));
 		
-		mockMvc.perform(put(baseUrl + existingId)
-						.content(mapper.writeValueAsString(requestObject)))
-				.andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+	}
 
+	@Override
+	public void assertGetCollectionResult(ResultActions action) throws Exception {
+		action.andExpect(jsonPath("$._embedded.nextNumberConstants").exists());
+	}
+
+	@Override
+	public void assertCreateWithPostResult(ResultActions action) throws Exception {
+		action
+			.andExpect(jsonPath("$.documentType").value(requestObject.get("documentType")))
+			.andExpect(jsonPath("$.resetNumber").value(requestObject.get("resetNumber")))
+			.andExpect(jsonPath("$.resetMethod").value(requestObject.get("resetMethod")))
+			.andExpect(jsonPath("$.includeMonthInNextNumber").value(requestObject.get("includeMonthInNextNumber")))
+			.andExpect(jsonPath("$.includeYearInNextNumber").value(requestObject.get("includeYearInNextNumber")));
+	}
+
+	@Override
+	public void assertUpdateWithPatchResult(ResultActions action, String beforeUpdateJson) throws Exception {
+		action
+			.andExpect(jsonPath("$.documentType").value(existingId))
+			.andExpect(jsonPath("$.resetNumber").value((Integer) JsonPath.read(beforeUpdateJson, "$.resetNumber")))
+			.andExpect(jsonPath("$.resetMethod").value(requestObject.get("resetMethod")))
+			.andExpect(jsonPath("$.includeMonthInNextNumber").value(requestObject.get("includeMonthInNextNumber")))
+			.andExpect(jsonPath("$.includeYearInNextNumber").value(requestObject.get("includeYearInNextNumber")));	
 	}
 
 	
