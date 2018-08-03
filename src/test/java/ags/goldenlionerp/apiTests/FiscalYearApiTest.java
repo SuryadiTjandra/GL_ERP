bb199abea3055225b2938d4b637984aae59361f5
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-08-03 15:16:05 +0700

    Mobve to new base class

--- src/test/java/ags/goldenlionerp/apiTests/FiscalYearApiTest.java
+++ src/test/java/ags/goldenlionerp/apiTests/FiscalYearApiTest.java
@@ -1,29 +1,29 @@
 package ags.goldenlionerp.apiTests;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.jayway.jsonpath.JsonPath;
 
 import ags.goldenlionerp.application.setups.fiscalyear.FiscalYearPK;
 
-public class FiscalYearApiTest extends ApiTestBase<FiscalYearPK> {
+public class FiscalYearApiTest extends ApiTestBaseNew<FiscalYearPK> {
 
 	@Override
-	Map<String, Object> requestObject() throws Exception {
+	protected Map<String, Object> requestObject() throws Exception {
 		Map<String, Object> map = new HashMap<>();
 		map.put("fiscalDatePattern", newId().getFiscalDatePattern());
 		map.put("fiscalYear", newId().getFiscalYear());
@@ -35,183 +35,54 @@ public class FiscalYearApiTest extends ApiTestBase<FiscalYearPK> {
 	}
 
 	@Override
-	String baseUrl() {
+	protected String baseUrl() {
 		return "/api/fiscalYears/";
 	}
 
 	@Override
-	FiscalYearPK existingId() {
+	protected FiscalYearPK existingId() {
 		return new FiscalYearPK("R", 2024);
 	}
 
 	@Override
-	FiscalYearPK newId() {
+	protected FiscalYearPK newId() {
 		return new FiscalYearPK("F", 2024);
-	}
-
-	
-	@Override @Test
-	public void getTestSingle() throws Exception {
-		mockMvc.perform(get(baseUrl + existingId).accept(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$.fiscalDatePattern").value(existingId.getFiscalDatePattern()))
-				.andExpect(jsonPath("$.fiscalYear").value(existingId.getFiscalYear()))
-				.andExpect(jsonPath("$.startDate").value(LocalDate.of(2024, 1, 1).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod1").value(LocalDate.of(2024, 1, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod3").value(LocalDate.of(2024, 3, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod8").value(LocalDate.of(2024, 8, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod12").value(LocalDate.of(2024, 12, 31).toString()))
-				.andExpect(jsonPath("$._links.extend.href").exists());
-	}
-
-	@Override @Test
-	public void getTestCollection() throws Exception {
-		mockMvc.perform(get(baseUrl).accept(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$._embedded.fiscalYears").exists());
-		
-	}
-
-	@Override @Test @Rollback
-	public void createTestWithPost() throws Exception {
-		assumeNotExists(baseUrl+newId);
-		
-		mockMvc.perform(post(baseUrl)
-						.accept(MediaType.APPLICATION_JSON)
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(mapper.writeValueAsString(requestObject)))
-				//.andDo(MockMvcResultHandlers.print())
-				.andExpect(MockMvcResultMatchers.status().isCreated());
-		
-		em.flush(); em.clear();
-		
-		String getRes = mockMvc.perform(get(baseUrl + newId))
-				.andExpect(jsonPath("$.fiscalDatePattern").value(newId.getFiscalDatePattern()))
-				.andExpect(jsonPath("$.fiscalYear").value(newId.getFiscalYear()))
-				.andExpect(jsonPath("$.startDate").value(LocalDate.of(2024, 6, 15).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod1").value(LocalDate.of(2024, 6, 30).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod2").value(LocalDate.of(2024, 7, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod3").value(LocalDate.of(2024, 8, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod4").value(LocalDate.of(2024, 9, 30).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod5").value(LocalDate.of(2024, 11, 23).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod6").value(LocalDate.of(2024, 11, 30).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod7").value(LocalDate.of(2024, 12, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod8").value(LocalDate.of(2025, 1, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod9").value(LocalDate.of(2025, 4, 12).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod10").value(LocalDate.of(2025, 6, 12).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod11").value(LocalDate.of(2025, 6, 12).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod12").value(LocalDate.of(2025, 6, 14).toString()))
-				.andReturn().getResponse().getContentAsString();
-				
-		assertCreationInfo(getRes);
-		
-	}
-
-	@Override @Test @Rollback
-	public void createTestWithPut() throws Exception {
-		assumeNotExists(baseUrl+newId);
-		
-		mockMvc.perform(put(baseUrl + newId)
-						.content(mapper.writeValueAsString(requestObject)))
-				.andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
-		
-	}
-
-	@Override @Test @Rollback
-	public void deleteTest() throws Exception {
-		assumeExists(baseUrl + existingId);
-		
-		mockMvc.perform(delete(baseUrl + existingId))
-			.andExpect(MockMvcResultMatchers.status().isNoContent());
-	
-		mockMvc.perform(get(baseUrl + existingId))
-			.andExpect(MockMvcResultMatchers.status().isNotFound());
-		
 	}
 
 	@Override @Test @Rollback
 	public void updateTestWithPatch() throws Exception {
-		assumeExists(baseUrl + existingId);
-		
-		String beforePatch = mockMvc.perform(get(baseUrl + existingId))
-								.andReturn().getResponse().getContentAsString();
-		
 		requestObject.remove("startDate");
 		requestObject.put("endDateOfPeriod5", LocalDate.of(2024, 5, 23));
 		requestObject.put("endDateOfPeriod9", LocalDate.of(2024, 9, 12));
 		requestObject.put("endDateOfPeriod10", LocalDate.of(2024, 10, 12));
 		
-		mockMvc.perform(patch(baseUrl + existingId)
-						.accept(MediaType.APPLICATION_JSON)
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(mapper.writeValueAsString(requestObject)))
-					.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-	
-		em.flush();	em.clear();
-		
-		String getResult = mockMvc.perform(get(baseUrl + existingId))
-				.andExpect(jsonPath("$.fiscalDatePattern").value(existingId.getFiscalDatePattern()))
-				.andExpect(jsonPath("$.fiscalYear").value(existingId.getFiscalYear()))
-				.andExpect(jsonPath("$.startDate").value(LocalDate.of(2024, 1, 1).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod1").value(LocalDate.of(2024, 1, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod2").value(LocalDate.of(2024, 2, 29).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod3").value(LocalDate.of(2024, 3, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod4").value(LocalDate.of(2024, 4, 30).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod5").value(LocalDate.of(2024, 5, 23).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod6").value(LocalDate.of(2024, 6, 30).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod7").value(LocalDate.of(2024, 7, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod8").value(LocalDate.of(2024, 8, 31).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod9").value(LocalDate.of(2024, 9, 12).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod10").value(LocalDate.of(2024, 10, 12).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod11").value(LocalDate.of(2024, 11, 30).toString()))
-				.andExpect(jsonPath("$.endDateOfPeriod12").value(LocalDate.of(2024, 12, 31).toString()))
-				.andReturn().getResponse().getContentAsString();
-		
-		assertUpdateInfo(getResult, beforePatch);
-		
+		super.updateTestWithPatch();
 	}
 	
-	@Test(expected=IllegalStateException.class) @Rollback
+	@Rule
+	public ExpectedException exp = ExpectedException.none();
+	
+	@Test @Rollback
 	public void updateTestWithPatchException() throws Exception {
-		assumeExists(baseUrl + existingId);
-		
-		mockMvc.perform(patch(baseUrl + existingId)
-				.accept(MediaType.APPLICATION_JSON)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(mapper.writeValueAsString(requestObject)))
-			.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-		
-		em.flush();	em.clear();
+		exp.expectCause(Matchers.instanceOf(IllegalStateException.class));
+		super.updateTestWithPatch();
 	}
 	
-	
-
-	@Override @Test @Rollback
-	public void updateTestWithPut() throws Exception {
-		assumeExists(baseUrl+existingId);
-		
-		mockMvc.perform(put(baseUrl + existingId)
-						.content(mapper.writeValueAsString(requestObject)))
-				.andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
-		
-	}
-
 	@Test @Rollback
 	public void extendTest() throws Exception {
 		createTestWithPost();
 		assumeExists(baseUrl+newId);
 		
-		String res = mockMvc.perform(get(baseUrl + newId)).andReturn().getResponse().getContentAsString();
+		String res = performer.performGet(baseUrl + newId).andReturn().getResponse().getContentAsString();
 		String extendLink = JsonPath.read(res, "$._links.extend.href");
 		
-		String location = mockMvc.perform(post(extendLink))
+		String location = performer.performPost(extendLink, null)
 				.andExpect(MockMvcResultMatchers.status().isCreated())
+				.andDo(print())
 				.andReturn().getResponse().getHeader("Location");
 		
 		
-		String extendRes = mockMvc.perform(get(location).accept(MediaType.APPLICATION_JSON))
+		String extendRes = performer.performGet(location)
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
 				.andExpect(jsonPath("$.fiscalDatePattern").value(newId.getFiscalDatePattern()))
@@ -239,10 +110,73 @@ public class FiscalYearApiTest extends ApiTestBase<FiscalYearPK> {
 		FiscalYearPK pk = new FiscalYearPK(existingId.getFiscalDatePattern(), existingId.getFiscalYear());
 		assumeExists(baseUrl+pk);
 		
-		String res = mockMvc.perform(get(baseUrl + pk)).andReturn().getResponse().getContentAsString();
+		String res = performer.performGet(baseUrl + pk).andReturn().getResponse().getContentAsString();
 		String extendLink = JsonPath.read(res, "$._links.extend.href");
 		
-		mockMvc.perform(post(extendLink))
+		performer.performPost(extendLink, null)
 				.andExpect(MockMvcResultMatchers.status().isConflict());
+	}
+
+	@Override
+	public void assertGetSingleResult(ResultActions action) throws Exception {
+		action
+			.andExpect(jsonPath("$.fiscalDatePattern").value(existingId.getFiscalDatePattern()))
+			.andExpect(jsonPath("$.fiscalYear").value(existingId.getFiscalYear()))
+			.andExpect(jsonPath("$.startDate").value(LocalDate.of(2024, 1, 1).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod1").value(LocalDate.of(2024, 1, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod3").value(LocalDate.of(2024, 3, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod8").value(LocalDate.of(2024, 8, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod12").value(LocalDate.of(2024, 12, 31).toString()))
+			.andExpect(jsonPath("$._links.extend.href").exists());
+		
+	}
+
+	@Override
+	public void assertGetCollectionResult(ResultActions action) throws Exception {
+		action
+			.andExpect(jsonPath("$._embedded.fiscalYears").exists());
+		
+	}
+
+	@Override
+	public void assertCreateWithPostResult(ResultActions action) throws Exception {
+		action
+			.andExpect(jsonPath("$.fiscalDatePattern").value(newId.getFiscalDatePattern()))
+			.andExpect(jsonPath("$.fiscalYear").value(newId.getFiscalYear()))
+			.andExpect(jsonPath("$.startDate").value(LocalDate.of(2024, 6, 15).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod1").value(LocalDate.of(2024, 6, 30).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod2").value(LocalDate.of(2024, 7, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod3").value(LocalDate.of(2024, 8, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod4").value(LocalDate.of(2024, 9, 30).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod5").value(LocalDate.of(2024, 11, 23).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod6").value(LocalDate.of(2024, 11, 30).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod7").value(LocalDate.of(2024, 12, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod8").value(LocalDate.of(2025, 1, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod9").value(LocalDate.of(2025, 4, 12).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod10").value(LocalDate.of(2025, 6, 12).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod11").value(LocalDate.of(2025, 6, 12).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod12").value(LocalDate.of(2025, 6, 14).toString()))
+			;
+	}
+
+	@Override
+	public void assertUpdateWithPatchResult(ResultActions action, String beforeUpdateJson) throws Exception {
+		action
+			.andExpect(jsonPath("$.fiscalDatePattern").value(existingId.getFiscalDatePattern()))
+			.andExpect(jsonPath("$.fiscalYear").value(existingId.getFiscalYear()))
+			.andExpect(jsonPath("$.startDate").value(LocalDate.of(2024, 1, 1).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod1").value(LocalDate.of(2024, 1, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod2").value(LocalDate.of(2024, 2, 29).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod3").value(LocalDate.of(2024, 3, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod4").value(LocalDate.of(2024, 4, 30).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod5").value(LocalDate.of(2024, 5, 23).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod6").value(LocalDate.of(2024, 6, 30).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod7").value(LocalDate.of(2024, 7, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod8").value(LocalDate.of(2024, 8, 31).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod9").value(LocalDate.of(2024, 9, 12).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod10").value(LocalDate.of(2024, 10, 12).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod11").value(LocalDate.of(2024, 11, 30).toString()))
+			.andExpect(jsonPath("$.endDateOfPeriod12").value(LocalDate.of(2024, 12, 31).toString()))
+			;
 	}
 }
