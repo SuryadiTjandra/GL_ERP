0889d759183d768c810ec252a5d7fae0d5983962
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-08-03 16:09:18 +0700

    Move from using JTA Transactional to Spring Transactional

--- src/test/java/ags/goldenlionerp/apiTests/ApiTestBase.java
+++ src/test/java/ags/goldenlionerp/apiTests/ApiTestBase.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
@@ -28,6 +27,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
