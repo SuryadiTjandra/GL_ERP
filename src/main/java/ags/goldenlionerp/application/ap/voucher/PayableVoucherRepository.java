93b218b10c7f57e42b7749c7e5b3695da0c226c4
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-08-23 13:46:46 +0700

    Add new kinds of repository interfaces for default behaviors on find and deletions

--- src/main/java/ags/goldenlionerp/application/ap/voucher/PayableVoucherRepository.java
+++ src/main/java/ags/goldenlionerp/application/ap/voucher/PayableVoucherRepository.java
@@ -11,43 +11,14 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import ags.goldenlionerp.entities.UndeleteableRepository;
+import ags.goldenlionerp.entities.VoidableRepository;
+
 @RepositoryRestResource(path="vouchers", collectionResourceRel="vouchers")
-public interface PayableVoucherRepository extends PagingAndSortingRepository<PayableVoucher, PayableVoucherPK> {
+public interface PayableVoucherRepository extends 
+	VoidableRepository<PayableVoucher, PayableVoucherPK>,
+	UndeleteableRepository<PayableVoucher, PayableVoucherPK> {
 
 	Collection<PayableVoucher> findByPkCompanyIdAndPkVoucherNumber(String companyId, int voucherNo);
 	
-	@Query("SELECT vou FROM PayableVoucher AS vou")
-	Page<PayableVoucher> findAllIncludeVoided(Pageable pageable);
-	
-	@Query("SELECT vou FROM PayableVoucher AS vou WHERE vou.pk = :id")
-	Optional<PayableVoucher> findIncludeVoided(@Param("id") PayableVoucherPK id);
-	
-	@Override
-	@Query("SELECT vou FROM PayableVoucher AS vou WHERE vou.voided = false")
-	Page<PayableVoucher> findAll(Pageable pageable);
-	
-	@Override
-	@Query("SELECT vou FROM PayableVoucher AS vou WHERE vou.voided = false AND vou.pk = :id")
-	Optional<PayableVoucher> findById(@Param("id") PayableVoucherPK id);
-		
-	
-	@Override @RestResource(exported=false)
-	default void delete(PayableVoucher entity) {
-		throw new UnsupportedOperationException("Vouchers must not be deleted!");
-	}
-	
-	@Override @RestResource(exported=false)
-	default void deleteById(PayableVoucherPK id) {
-		throw new UnsupportedOperationException("Vouchers must not be deleted!");
-	}
-	
-	@Override @RestResource(exported=false)
-	default void deleteAll() {
-		throw new UnsupportedOperationException("Vouchers must not be deleted!");
-	}
-	
-	@Override @RestResource(exported=false)
-	default void deleteAll(Iterable<? extends PayableVoucher> entities) {
-		throw new UnsupportedOperationException("Vouchers must not be deleted!");
-	}
 }
--- src/main/java/ags/goldenlionerp/application/ar/invoice/ReceivableInvoiceRepository.java
+++ src/main/java/ags/goldenlionerp/application/ar/invoice/ReceivableInvoiceRepository.java
@@ -1,53 +1,17 @@
 package ags.goldenlionerp.application.ar.invoice;
 
 import java.util.Collection;
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+import ags.goldenlionerp.entities.UndeleteableRepository;
+import ags.goldenlionerp.entities.VoidableRepository;
 
 @RepositoryRestResource(collectionResourceRel="invoices", path="invoices")
-public interface ReceivableInvoiceRepository extends PagingAndSortingRepository<ReceivableInvoice, ReceivableInvoicePK> {
+public interface ReceivableInvoiceRepository extends 
+	VoidableRepository<ReceivableInvoice, ReceivableInvoicePK>, 
+	UndeleteableRepository<ReceivableInvoice, ReceivableInvoicePK> {
 
+	
 	Collection<ReceivableInvoice> findByPkCompanyIdAndPkInvoiceNumber(String companyId, int invoiceNo);
 	
-	@Query("SELECT inv FROM ReceivableInvoice AS inv")
-	Page<ReceivableInvoice> findAllIncludeVoided(Pageable pageable);
-	
-	@Query("SELECT inv FROM ReceivableInvoice AS inv WHERE inv.pk = :id")
-	Optional<ReceivableInvoice> findIncludeVoided(@Param("id") ReceivableInvoicePK id);
-	
-	@Override
-	@Query("SELECT inv FROM ReceivableInvoice AS inv WHERE inv.voided = false")
-	Page<ReceivableInvoice> findAll(Pageable pageable);
-	
-	@Override
-	@Query("SELECT inv FROM ReceivableInvoice AS inv WHERE inv.voided = false AND inv.pk = :id")
-	Optional<ReceivableInvoice> findById(@Param("id") ReceivableInvoicePK id);
-		
-	
-	@Override @RestResource(exported=false)
-	default void delete(ReceivableInvoice entity) {
-		throw new UnsupportedOperationException("Invoices must not be deleted!");
-	}
-	
-	@Override @RestResource(exported=false)
-	default void deleteById(ReceivableInvoicePK id) {
-		throw new UnsupportedOperationException("Invoices must not be deleted!");
-	}
-	
-	@Override @RestResource(exported=false)
-	default void deleteAll() {
-		throw new UnsupportedOperationException("Invoices must not be deleted!");
-	}
-	
-	@Override @RestResource(exported=false)
-	default void deleteAll(Iterable<? extends ReceivableInvoice> entities) {
-		throw new UnsupportedOperationException("Invoices must not be deleted!");
-	}
+
 }
