3ed41089c8ebd4697c5f2cd54cd096cfc20a4c6b
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-09-03 12:13:30 +0700

    PurchaseOrder add more tests

--- src/test/java/ags/goldenlionerp/apiTests/purchase/PurchaseOrderApiTest.java
+++ src/test/java/ags/goldenlionerp/apiTests/purchase/PurchaseOrderApiTest.java
@@ -13,8 +13,12 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.hamcrest.Matchers;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.ResultActions;
+
+import com.jayway.jsonpath.JsonPath;
 
 import ags.goldenlionerp.apiTests.ApiTestBase;
 import ags.goldenlionerp.application.addresses.address.AddressBookMaster;
@@ -24,6 +28,9 @@ import ags.goldenlionerp.application.setups.company.Company;
 import ags.goldenlionerp.application.setups.company.CompanyRepository;
 import ags.goldenlionerp.application.setups.taxcode.TaxRule;
 import ags.goldenlionerp.application.setups.taxcode.TaxRuleRepository;
+import ags.goldenlionerp.masterdata.itemLocation.ItemLocation;
+import ags.goldenlionerp.masterdata.itemmaster.ItemMaster;
+import ags.goldenlionerp.masterdata.itemmaster.ItemMasterRepository;
 
 public class PurchaseOrderApiTest extends ApiTestBase<PurchaseOrderPK> {
 
@@ -31,6 +38,7 @@ public class PurchaseOrderApiTest extends ApiTestBase<PurchaseOrderPK> {
 	@Autowired private CompanyRepository compRepo;
 	@Autowired private AddressBookRepository addrRepo;
 	@Autowired private TaxRuleRepository taxRepo;
+	@Autowired private ItemMasterRepository itemRepo;
 	
 	@Override
 	protected Map<String, Object> requestObject() throws Exception {
@@ -67,8 +75,8 @@ public class PurchaseOrderApiTest extends ApiTestBase<PurchaseOrderPK> {
 		det1.put("orderQuantity", 10);
 		det1.put("unitOfMeasure", "IN");
 		det1.put("extendedUnitOfMeasure", "CM");
-		det1.put("unitCost", BigDecimal.valueOf(50.0));
-		det1.put("extendedUnitCost", BigDecimal.valueOf(30));
+		det1.put("unitCost", BigDecimal.valueOf(30));
+		det1.put("extendedUnitCost", BigDecimal.valueOf(50));
 		det1.put("unitDiscountCode", "D00");
 		//det1.put("jobNo")
 		det1.put("landedCostRule", "LOC");
@@ -154,7 +162,6 @@ public class PurchaseOrderApiTest extends ApiTestBase<PurchaseOrderPK> {
 			.andExpect(jsonPath("$.purchaseOrderNumber").value(newId.getPurchaseOrderNumber()))
 			.andExpect(jsonPath("$.purchaseOrderType").value(newId.getPurchaseOrderType()))
 			
-			.andExpect(jsonPath("$.orderDate").value(requestObject.get("orderDate").toString()))
 			.andExpect(jsonPath("$.businessUnitId").value(requestObject.get("businessUnitId")))
 			.andExpect(jsonPath("$.vendorId").value(requestObject.get("vendorId")))
 			.andExpect(jsonPath("$.receiverId").value(requestObject.getOrDefault("receiverId", requestObject.get("vendorId"))))
@@ -173,7 +180,7 @@ public class PurchaseOrderApiTest extends ApiTestBase<PurchaseOrderPK> {
 			.andExpect(jsonPath("$.taxCode").value(requestObject.getOrDefault("taxCode", "")))
 			.andExpect(jsonPath("$.taxAllowance").value(tax.map(TaxRule::getTaxAllowance).orElse(false)))
 			//.andExpect(jsonPath("$.taxPercentage").value(taxRate))
-			.andExpect(jsonPath("$.$discountCode").value(requestObject.getOrDefault("discountCode", "")))
+			.andExpect(jsonPath("$.discountCode").value(requestObject.getOrDefault("discountCode", "")))
 			//.andExpect(jsonPath("$.discountRate").value(requestObject.getOrDefault("discountRate", calculateDiscount())))
 			.andExpect(jsonPath("$.lastStatus").value("220")) //from where?
 			.andExpect(jsonPath("$.nextStatus").value("400")) //from whrre?
@@ -184,9 +191,154 @@ public class PurchaseOrderApiTest extends ApiTestBase<PurchaseOrderPK> {
 			.andExpect(jsonPath("$.vehicleRegistrationNumber").value(requestObject.getOrDefault("vehicleRegistrationType", "")))
 			.andExpect(jsonPath("$.vehicleType").value(requestObject.getOrDefault("vehicleType", "")))
 			.andExpect(jsonPath("$.vehicleDescription").value(requestObject.getOrDefault("vehicleDescription", "")))
-			.andExpect(jsonPath("$.vehicleDescription2").value(requestObject.getOrDefault("vehicleDescription2", "")))
+			.andExpect(jsonPath("$.vehicleDescription2").value(requestObject.getOrDefault("vehicleDescription2", "")));
 			
+		@SuppressWarnings("unchecked")
+		List<Map<String, Object>> list = (List<Map<String, Object>>) requestObject.get("details");
+		Map<String, Object> det0 = list.get(0);
+		Map<String, Object> det1 = list.get(1);
+		ItemMaster item0 = itemRepo.findById((String) det0.get("itemNumber")).get();
+		ItemMaster item1 = itemRepo.findById((String) det1.get("itemNumber")).get();
+		ItemLocation primLoc0 = item0.getItemLocations().stream()
+								.filter(il -> il.getPk().getBusinessUnitId().equals(requestObject.get("businessUnit")))
+								.filter(il -> il.getLocationStatus() == "P")
+								.findAny().get();
+		ItemLocation primLoc1 = item1.getItemLocations().stream()
+								.filter(il -> il.getPk().getBusinessUnitId().equals(requestObject.get("businessUnit")))
+								.filter(il -> il.getLocationStatus() == "P")
+								.findAny().get();
+		
+		action
+			.andExpect(jsonPath("$.details[0].companyId").value(newId.getCompanyId()))
+			.andExpect(jsonPath("$.details[1].companyId").value(newId.getCompanyId()))
+			.andExpect(jsonPath("$.details[0].purchaseOrderNumber").value(newId.getPurchaseOrderNumber()))
+			.andExpect(jsonPath("$.details[1].purchaseOrderNumber").value(newId.getPurchaseOrderNumber()))
+			.andExpect(jsonPath("$.details[0].purchaseOrderType").value(newId.getPurchaseOrderType()))
+			.andExpect(jsonPath("$.details[1].purchaseOrderType").value(newId.getPurchaseOrderType()))
+			.andExpect(jsonPath("$.details[0].purchaseOrderSequence").value(10))
+			.andExpect(jsonPath("$.details[1].purchaseOrderSequence").value(20))
+			
+			.andExpect(jsonPath("$.details[0].businessUnitId").value(requestObject.get("businessUnitId")))
+			.andExpect(jsonPath("$.details[0].vendorId").value(requestObject.get("vendorId")))
+			.andExpect(jsonPath("$.details[0].receiverId").value(requestObject.getOrDefault("receiverId", requestObject.get("vendorId"))))
+			.andExpect(jsonPath("$.details[0].customerId").value(requestObject.getOrDefault("customerId", "")))
+			.andExpect(jsonPath("$.details[1].businessUnitId").value(requestObject.get("businessUnitId")))
+			.andExpect(jsonPath("$.details[1].vendorId").value(requestObject.get("vendorId")))
+			.andExpect(jsonPath("$.details[1].receiverId").value(requestObject.getOrDefault("receiverId", requestObject.get("vendorId"))))
+			.andExpect(jsonPath("$.details[1].customerId").value(requestObject.getOrDefault("customerId", "")))
+			
+			.andExpect(jsonPath("$.details[0].itemNumber").value(det0.get("itemNumber")))
+			.andExpect(jsonPath("$.details[0].locationId").value(det0.getOrDefault("locationId", primLoc0.getPk().getLocationId())))
+			.andExpect(jsonPath("$.details[0].serialLotNumber").value(det0.getOrDefault("serialLotNumber", primLoc0.getPk().getSerialLotNo())))
+			.andExpect(jsonPath("$.details[0].description").value(item0.getDescription()))
+			.andExpect(jsonPath("$.details[0].lineType").value(item0.getTransactionType()))
+			.andExpect(jsonPath("$.details[1].itemNumber").value(det1.get("itemNumber")))
+			.andExpect(jsonPath("$.details[1].locationId").value(det1.getOrDefault("locationId", primLoc1.getPk().getLocationId())))
+			.andExpect(jsonPath("$.details[1].serialLotNumber").value(det1.getOrDefault("serialLotNumber", primLoc1.getPk().getSerialLotNo())))
+			.andExpect(jsonPath("$.details[1].description").value(item1.getDescription()))
+			.andExpect(jsonPath("$.details[1].lineType").value(item1.getTransactionType()))
+			
+			.andExpect(jsonPath("$.details[0].quantity").value(det0.get("quantity")))
+			.andExpect(jsonPath("$.details[0].unitOfMeasure").value(det0.getOrDefault("unitOfMeasure", item0.getUnitsOfMeasure().getPrimaryUnitOfMeasure())))
+			.andExpect(jsonPath("$.details[0].extendedQuantity").value(det0.getOrDefault("extendedQuantity", 0)))
+			.andExpect(jsonPath("$.details[0].extendedUnitOfMeasure").value(det0.getOrDefault("extendedUnitOfMeasure", "")))
+			.andExpect(jsonPath("$.details[0].orderQuantity").value(det0.get("quantity")))
+			.andExpect(jsonPath("$.details[0].receivedQuantity").value(0))
+			.andExpect(jsonPath("$.details[0].cancelledQuantity").value(0))
+			.andExpect(jsonPath("$.details[0].returnedQuantity").value(0))
+			.andExpect(jsonPath("$.details[0].openQuantity").value(det0.get("quantity")))
+			.andExpect(jsonPath("$.details[0].unitConversionFactor").value(1))
+			.andExpect(jsonPath("$.details[0].extendedUnitConversionFactor").value(0))
+			.andExpect(jsonPath("$.details[0].primaryOrderQuantity").value(det0.get("quantity")))
+			.andExpect(jsonPath("$.details[0].primaryUnitOfMeasure").value(item0.getUnitsOfMeasure().getPrimaryUnitOfMeasure()))
+			//.andExpect(jsonPath("$.details[0].secondaryOrderQuantity").value(matcher))
+			.andExpect(jsonPath("$.details[0].secondaryUnitOfMeasure").value(item0.getUnitsOfMeasure().getSecondaryUnitOfMeasure()))
+			
+			.andExpect(jsonPath("$.details[1].quantity").value(det1.get("quantity")))
+			.andExpect(jsonPath("$.details[1].unitOfMeasure").value(det1.getOrDefault("unitOfMeasure", item0.getUnitsOfMeasure().getPrimaryUnitOfMeasure())))
+			.andExpect(jsonPath("$.details[1].extendedQuantity").value(25.4/*det1.getOrDefault("extendedQuantity", 0)*/))
+			.andExpect(jsonPath("$.details[1].extendedUnitOfMeasure").value(det1.getOrDefault("extendedUnitOfMeasure", "")))
+			.andExpect(jsonPath("$.details[1].orderQuantity").value(det1.get("quantity")))
+			.andExpect(jsonPath("$.details[1].receivedQuantity").value(0))
+			.andExpect(jsonPath("$.details[1].cancelledQuantity").value(0))
+			.andExpect(jsonPath("$.details[1].returnedQuantity").value(0))
+			.andExpect(jsonPath("$.details[1].openQuantity").value(det1.get("quantity")))
+			.andExpect(jsonPath("$.details[1].unitConversionFactor").value(0.083333333))
+			.andExpect(jsonPath("$.details[1].extendedUnitConversionFactor").value(0.032808398))
+			.andExpect(jsonPath("$.details[1].primaryOrderQuantity").value(0.83333/*det1.get("quantity")*/))
+			.andExpect(jsonPath("$.details[1].primaryUnitOfMeasure").value(item1.getUnitsOfMeasure().getPrimaryUnitOfMeasure()))
+			//.andExpect(jsonPath("$.details[1].secondaryOrderQuantity").value(matcher))
+			.andExpect(jsonPath("$.details[1].secondaryUnitOfMeasure").value(item1.getUnitsOfMeasure().getSecondaryUnitOfMeasure()))
+			
+			.andExpect(jsonPath("$.details[0].unitCost").value(det0.get("unitCost")))
+			.andExpect(jsonPath("$.details[0].extendedUnitCost").value(0))
+			.andExpect(jsonPath("$.details[0].extendedCost").value(500.00))
+			.andExpect(jsonPath("$.details[1].unitCost").value(127))
+			.andExpect(jsonPath("$.details[1].extendedUnitCost").value(det0.get("extendedUnitCost")))
+			.andExpect(jsonPath("$.details[1].extendedCost").value(1270.00))
+			
+			.andExpect(jsonPath("$.details[0].baseCurrency").value(comp.getCurrencyCodeBase()))
+			.andExpect(jsonPath("$.details[0].transactionCurrency").value(requestObject.getOrDefault("transactionCurrency", addr.getApSetting().get().getCurrencyCodeTransaction())))
+			.andExpect(jsonPath("$.details[0].exchangeRate").value(requestObject.getOrDefault("exchangeRate", 1.00)))
+			.andExpect(jsonPath("$.details[1].baseCurrency").value(comp.getCurrencyCodeBase()))
+			.andExpect(jsonPath("$.details[1].transactionCurrency").value(requestObject.getOrDefault("transactionCurrency", addr.getApSetting().get().getCurrencyCodeTransaction())))
+			.andExpect(jsonPath("$.details[1].exchangeRate").value(requestObject.getOrDefault("exchangeRate", 1.00)))
+			
+			.andExpect(jsonPath("$.details[0].taxableAmount").value(0))
+			.andExpect(jsonPath("$.details[1].taxableAmount").value(0))
+			
+			.andExpect(jsonPath("$.details[0].glClass").value(item0.getGlClass()))
+			.andExpect(jsonPath("$.details[1].glClass").value(item0.getGlClass()))
+			.andExpect(jsonPath("$.details[0].lastStatus").value("220"))
+			.andExpect(jsonPath("$.details[1].lastStatus").value("220"))
+			.andExpect(jsonPath("$.details[0].nextStatus").value("400"))
+			.andExpect(jsonPath("$.details[1].nextStatus").value("400"))
+			
+			.andExpect(jsonPath("$.details[0].orderDate").value(requestObject.get("orderDate")))
+			.andExpect(jsonPath("$.details[0].requestDate").value(requestObject.getOrDefault("requestDate", requestObject.get("orderDate"))))
+			.andExpect(jsonPath("$.details[0].promisedDeliveryDate").value(requestObject.get("promisedDeliveryDate")))
+			.andExpect(jsonPath("$.details[0].receiptDate", Matchers.nullValue()))
+			.andExpect(jsonPath("$.details[0].closedDate", Matchers.nullValue()))
+			.andExpect(jsonPath("$.details[0].glDate", Matchers.nullValue()))
+			.andExpect(jsonPath("$.details[1].orderDate").value(requestObject.get("orderDate")))
+			.andExpect(jsonPath("$.details[1].requestDate").value(requestObject.getOrDefault("requestDate", requestObject.get("orderDate"))))
+			.andExpect(jsonPath("$.details[1].promisedDeliveryDate").value(requestObject.get("promisedDeliveryDate")))
+			.andExpect(jsonPath("$.details[1].receiptDate", Matchers.nullValue()))
+			.andExpect(jsonPath("$.details[1].closedDate", Matchers.nullValue()))
+			.andExpect(jsonPath("$.details[1].glDate", Matchers.nullValue()))
+			
+			.andExpect(jsonPath("$.details[0].paymentTermCode").value(requestObject.getOrDefault("paymentTermCode", addr.getApSetting().get().getPaymentTermCode())))
+			.andExpect(jsonPath("$.details[0].taxCode").value(requestObject.getOrDefault("taxCode", "")))
+			.andExpect(jsonPath("$.details[0].taxAllowance").value(tax.map(TaxRule::getTaxAllowance).orElse(false)))
+			//.andExpect(jsonPath("$.details[0].taxPercentage").value(taxRate))
+			.andExpect(jsonPath("$.details[0].$discountCode").value(requestObject.getOrDefault("discountCode", "")))
+			//.andExpect(jsonPath("$.details[0].discountRate").value(requestObject.getOrDefault("discountRate", calculateDiscount())))
+			.andExpect(jsonPath("$.details[0].$unitDiscountCode").value(det0.getOrDefault("unitDiscountCode", "")))
+			//.andExpect(jsonPath("$.details[0].$unitDiscountRate").value(requestObject.getOrDefault("discountCode", "")))
+			
+			.andExpect(jsonPath("$.details[1].paymentTermCode").value(requestObject.getOrDefault("paymentTermCode", addr.getApSetting().get().getPaymentTermCode())))
+			.andExpect(jsonPath("$.details[1].taxCode").value(requestObject.getOrDefault("taxCode", "")))
+			.andExpect(jsonPath("$.details[1].taxAllowance").value(tax.map(TaxRule::getTaxAllowance).orElse(false)))
+			//.andExpect(jsonPath("$.details[1].taxPercentage").value(taxRate))
+			.andExpect(jsonPath("$.details[1].discountCode").value(requestObject.getOrDefault("discountCode", "")))
+			//.andExpect(jsonPath("$.details[1].discountRate").value(requestObject.getOrDefault("discountRate", calculateDiscount())))
+			.andExpect(jsonPath("$.details[1].$unitDiscountCode").value(det1.getOrDefault("unitDiscountCode", "")))
+			//.andExpect(jsonPath("$.details[1].$unitDiscountRate").value(requestObject.getOrDefault("discountCode", "")))		
+			
+			.andExpect(jsonPath("$.details[0].landedCostRule").value(det0.getOrDefault("landedCostRule" ,"")))
+			.andExpect(jsonPath("$.details[0].containerSize").value(det0.getOrDefault("containerSize", "")))
+			.andExpect(jsonPath("$.details[0].stuffingType").value(det0.getOrDefault("stuffingType", "")))
+			.andExpect(jsonPath("$.details[0].containerLoadType").value(det0.getOrDefault("containerLoadType", "")))
+			.andExpect(jsonPath("$.details[1].landedCostRule").value(det1.getOrDefault("landedCostRule" ,"")))
+			.andExpect(jsonPath("$.details[1].containerSize").value(det1.getOrDefault("containerSize", "")))
+			.andExpect(jsonPath("$.details[1].stuffingType").value(det1.getOrDefault("stuffingType", "")))
+			.andExpect(jsonPath("$.details[1].containerLoadType").value(det1.getOrDefault("containerLoadType", "")))
 			;
+			
+			String json = action.andReturn().getResponse().getContentAsString();
+			assertCreationInfo(JsonPath.read(json, "$.details[0]"));
+			assertCreationInfo(JsonPath.read(json, "$.details[1]"));
+			
 	}
 	
 	
@@ -195,14 +347,26 @@ public class PurchaseOrderApiTest extends ApiTestBase<PurchaseOrderPK> {
 		return null;
 	}
 
-	@Override
+	@Override @Test @Rollback
 	public void updateTestWithPatch() throws Exception {
 		assumeExists(baseUrl + existingId);
 		performer.performPatch(baseUrl + existingId, requestObject)
 				.andExpect(status().isMethodNotAllowed());
 	}
 	
-	@Override
+	@Test @Rollback
+	public void updateTestWithPost() throws Exception {
+		assumeExists(baseUrl + existingId);
+		
+		requestObject.put("companyId", existingId.getCompanyId());
+		requestObject.put("purchaseOrderNumber", existingId().getPurchaseOrderNumber());
+		requestObject.put("purchaseOrderType", existingId().getPurchaseOrderType());
+		
+		performer.performPost(baseUrl, requestObject)
+				.andExpect(status().isConflict());
+	}
+	
+	@Override @Test @Rollback
 	public void deleteTest() throws Exception {
 		assumeExists(baseUrl + existingId);
 		performer.performDelete(baseUrl + existingId)
