6e69910e426d81ae59935de94bc8dc27eb13ddd6
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-09-05 12:56:48 +0700

    Base for purchaseorder page

---
+++ src/main/java/ags/goldenlionerp/application/purchase/PurchasePageController.java
@@ -0,0 +1,14 @@
+package ags.goldenlionerp.application.purchase;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PurchasePageController {
+
+	@GetMapping("/purchaseOrder") @ResponseBody
+	public String purchaseOrderPage() {
+		return "purchaseOrder";
+	}
+}
