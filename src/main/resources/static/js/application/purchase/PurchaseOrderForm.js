73c4ccf10a98005cde175ef3172f5a53224cef87
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-09-13 14:40:21 +0700

    Added more fields for the form. Added behaviors for when selected vendor changes

--- src/main/resources/static/js/application/purchase/PurchaseOrderForm.js
+++ src/main/resources/static/js/application/purchase/PurchaseOrderForm.js
@@ -1,5 +1,6 @@
 import DataCodeInput from "../../baseComponents/inputs/DataCodeInput.js";
 import ResourceInput from "../../baseComponents/inputs/ResourceInput.js";
+import AJAXPerformer from "/js/util/AJAXPerformer.js";
 
 var PurchaseOrderForm = {
 		components: {
@@ -19,7 +20,7 @@ var PurchaseOrderForm = {
 		<b-form>
 			<b-form-row>
 				<b-col cols="6">
-					<b-form-group label="No. Order" horizontal label-size="sm">
+					<b-form-group label="No. Order" horizontal label-size="sm" label-text-align="right">
 						<b-form-input placeholder="Nomor Order" type="number" required size="sm" style="width:65%;display:inline"
 							v-model="formItem.purchaseOrderNumber" 
 							:readOnly="!editable"
@@ -30,7 +31,7 @@ var PurchaseOrderForm = {
 							:readOnly="!editable">
 						</DataCodeInput>
 					</b-form-group>
-					<b-form-group label="Unit Kerja" horizontal label-size="sm">
+					<b-form-group label="Unit Usaha" horizontal label-size="sm" label-text-align="right">
 						<ResourceInput  style="display:inline" size="sm"
 							v-model="formItem.companyId"
 							:readOnly="!editable"
@@ -42,7 +43,7 @@ var PurchaseOrderForm = {
 							>
 						</ResourceInput>
 					</b-form-group>
-					<b-form-group label="Unit Kerja" horizontal label-size="sm">
+					<b-form-group label="Unit Kerja" horizontal label-size="sm" label-text-align="right">
 						<ResourceInput size="sm"
 							v-model="formItem.businessUnitId"
 							:readOnly="!editable"
@@ -53,20 +54,14 @@ var PurchaseOrderForm = {
 								descPath:'description'}"
 							>
 						</ResourceInput>
-						<!--
-						<b-form-input type="text" required
-							v-model="formItem.businessUnitId"
-							:readOnly="!editable">
-						</b-form-input>
-						-->
 					</b-form-group>
-					<b-form-group label="Tanggal Order" horizontal label-size="sm">
+					<b-form-group label="Tanggal Order" horizontal label-size="sm" label-text-align="right">
 						<b-form-input type="date" required size="sm"
 							v-model="formItem.orderDate"
 							:readOnly="!editable">
 						</b-form-input>
 					</b-form-group>
-					<b-form-group label="Est. Tanggal Terima" horizontal label-size="sm">
+					<b-form-group label="Est. Tanggal Terima" horizontal label-size="sm" label-text-align="right">
 						<b-form-input type="date" size="sm"
 							v-model="formItem.promisedDeliveryDate"
 							:readOnly="!editable">
@@ -74,7 +69,7 @@ var PurchaseOrderForm = {
 					</b-form-group>
 				</b-col>
 				<b-col>
-					<b-form-group label="Supplier" label-size="sm" horizontal>
+					<b-form-group label="Supplier" label-size="sm" horizontal label-text-align="right">
 						<ResourceInput size="sm"
 							v-model="formItem.vendorId"
 							:readOnly="!editable"
@@ -87,7 +82,7 @@ var PurchaseOrderForm = {
 							>
 						</ResourceInput>
 					</b-form-group>
-					<b-form-group label="Alamat Kirim" label-size="sm" horizontal>
+					<b-form-group label="Alamat Kirim" label-size="sm" horizontal label-text-align="right">
 						<ResourceInput size="sm"
 							v-model="formItem.receiverId"
 							:readOnly="!editable"
@@ -103,6 +98,67 @@ var PurchaseOrderForm = {
 						<p class="mb-0">{{receiver == null ? "" : receiver.currentAddress.address2}}</p>
 						<p class="mb-0">{{receiver == null ? "" : receiver.currentAddress.address3}}</p>
 						<p class="mb-0">{{receiver == null ? "" : receiver.currentAddress.address4}}</p>
+					</b-form-group>
+				</b-col>
+			</b-form-row>
+			<b-form-row>
+				<b-col>
+					<b-form-group label="Mata Uang" label-size="sm" horizontal label-text-align="right">
+						<DataCodeInput productCode="00" systemCode="CC" size="sm"
+							v-model="formItem.transactionCurrency"
+							:readOnly="!editable">
+						</DataCodeInput>
+					</b-form-group>
+					<b-form-group label="Tipe Pembayaran" label-size="sm" horizontal label-text-align="right">
+						<ResourceInput size="sm"
+							v-model="formItem.paymentTermCode"
+							:readOnly="!editable"
+							:resourceMetadata="{
+								apiUrl:'/api/paymentTerms',
+								dataPath:'paymentTerms', 
+								idPath:'paymentTermCode', 
+								descPath:'description'
+							}">
+						</ResourceInput>
+					</b-form-group>
+					<b-form-group label="Keterangan" label-size="sm" horizontal label-text-align="right">
+						<b-input type="text" size="sm" placeholder="Keterangan"
+							:readOnly="!editable"
+							v-model="formItem.description">
+						</b-input>
+					</b-form-group>
+				</b-col>
+				<b-col>
+					<b-form-group label="Kurs" label-size="sm" horizontal label-text-align="right">
+						<b-input type="number" size="sm" placeholder="Kurs"
+							:readOnly="!editable"
+							:formatter="num => Number(num).toFixed(2)"
+							v-model="formItem.exchangeRate">
+						</b-input>
+					</b-form-group>
+					<b-form-group label="Tipe PPN" label-size="sm" horizontal label-text-align="right">
+						<ResourceInput size="sm"
+							v-model="formItem.taxCode"
+							:readOnly="!editable"
+							:resourceMetadata="{
+								apiUrl:'/api/taxRules',
+								dataPath:'taxRules', 
+								idPath:'taxCode', 
+								descPath:'description'
+							}">
+						</ResourceInput>
+					</b-form-group>
+					<b-form-group label="Customer" label-size="sm" horizontal label-text-align="right">
+						<ResourceInput size="sm"
+							v-model="formItem.vendorId"
+							:readOnly="!editable"
+							:resourceMetadata="{
+								apiUrl:'/api/addresses',
+								dataPath:'addresses', 
+								idPath:'addressNumber', 
+								descPath:'name'}"
+							>
+						</ResourceInput>
 					</b-form-group>
 				</b-col>
 			</b-form-row>
@@ -145,9 +201,27 @@ var PurchaseOrderForm = {
 			}
 		},
 		methods: {
-			onVendorChange: function(vendorId, vendor){
-				this.vendorId = vendorId;
+			onVendorChange: function(vendorId, vendor){				
+				this.formItem.vendorId = vendorId;
 				this.vendor = vendor;
+				if (vendor == null)
+					return;
+				
+				if (this.receiver == null){
+					this.formItem.receiverId = vendorId;
+					this.receiver = vendor;
+				}
+				if (this.formItem.transactionCurrency == null){
+					AJAXPerformer.getAsJson(vendor._links.apSetting.href)
+						.then(res => this.formItem.transactionCurrency = res.currencyCodeTransaction)
+					//this.formItem.transactionCurrency = vendor.apSetting.currencyCodeTransaction;
+				}
+					
+				if (this.formItem.paymentTermCode == null)
+					AJAXPerformer.getAsJson(vendor._links.apSetting.href)
+						.then(res => this.formItem.paymentTermCode = res.paymentTermCode)
+					//this.formItem.paymentTermCode = vendor.apSetting.paymentTermCode;
+				
 			},
 			onReceiverChange: function(receiverId, receiver){
 				this.receiverId = receiverId;
