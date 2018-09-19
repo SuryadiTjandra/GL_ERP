57cb75f71cd6691e8947043fe30bf267e1c9f8f5
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-09-19 15:31:51 +0700

    SalesOrder front-end

---
+++ src/main/resources/static/js/application/sales/SalesOrderForm.js
@@ -0,0 +1,11 @@
+//import BasicForm from "./PurchaseOrderFormBasicInfo.js";
+//import ItemForm from "./PurchaseOrderFormItemList.js";
+//import ShippingForm from "./PurchaseOrderFormShippingInfo.js";
+//import Summary from "./PurchaseOrderSummary.js";
+import AJAXPerformer from "/js/util/AJAXPerformer.js";
+
+var SalesOrderForm = {
+		template: '<span>Form</span>'
+}
+
+export default SalesOrderForm;
