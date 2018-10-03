d005de8db8024836a551d8d16444e744f5dc39ad
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-10-03 15:27:38 +0700

    organize unit of measure test

--- src/test/java/ags/goldenlionerp/application/item/uomconversion/StandardUomConversionServiceTest.java
+++ src/test/java/ags/goldenlionerp/application/item/uomconversion/StandardUomConversionServiceTest.java
@@ -24,24 +24,62 @@ public class StandardUomConversionServiceTest {
 		Optional<BigDecimal> res1 = service.findConversionValue("MT", "MT");
 		assertEquals(1, res1.get().intValueExact());
 		
-		Optional<BigDecimal> res2 = service.findConversionValue("IN", "CM");
-		assertEquals(2.54, res2.get().doubleValue(), 0.001);
+		//Optional<BigDecimal> res2 = service.findConversionValue("IN", "CM");
+		//assertEquals(2.54, res2.get().doubleValue(), 0.001);
 		
-		Optional<BigDecimal> res3 = service.findConversionValue("GM", "KG");
-		assertEquals(0.001, res3.get().doubleValue(), 0.001);
+		//Optional<BigDecimal> res3 = service.findConversionValue("GM", "KG");
+		//assertEquals(0.001, res3.get().doubleValue(), 0.001);
 		
-		Optional<BigDecimal> res4 = service.findConversionValue("KM", "MM");
-		assertEquals(1000000, res4.get().intValue());
+		//Optional<BigDecimal> res4 = service.findConversionValue("KM", "MM");
+		//assertEquals(1000000, res4.get().intValue());
 		
-		Optional<BigDecimal> res5 = service.findConversionValue("OZ", "KG");
-		assertEquals(0.02835, res5.get().doubleValue(), 0.001);
+		//Optional<BigDecimal> res5 = service.findConversionValue("OZ", "KG");
+		//assertEquals(0.02835, res5.get().doubleValue(), 0.001);
 		
-		Optional<BigDecimal> res6 = service.findConversionValue("CM", "KG");
-		assertEquals(Optional.empty(), res6);
+		//Optional<BigDecimal> res6 = service.findConversionValue("CM", "KG");
+		//assertEquals(Optional.empty(), res6);
 		
-		Optional<BigDecimal> res7 = service.findConversionValue("CM", "FT");
-		assertEquals(0.0328084, res7.get().doubleValue(), 0.001);
+		//Optional<BigDecimal> res7 = service.findConversionValue("CM", "FT");
+		//assertEquals(0.0328084, res7.get().doubleValue(), 0.001);
 		
 	}
+	
+	@Test
+	public void test1() {
+		Optional<BigDecimal> res2 = service.findConversionValue("IN", "CM");
+		assertEquals(2.54, res2.get().doubleValue(), 0.001);
+	}
+	
+	@Test
+	public void test2() {
+		Optional<BigDecimal> res3 = service.findConversionValue("GM", "KG");
+		assertEquals(0.001, res3.get().doubleValue(), 0.001);
+	}
 
+	
+	@Test
+	public void test3() {
+		Optional<BigDecimal> res4 = service.findConversionValue("KM", "MM");
+		assertEquals(1000000, res4.get().intValue());
+	}
+
+	
+	@Test
+	public void test4() {
+		Optional<BigDecimal> res5 = service.findConversionValue("OZ", "KG");
+		assertEquals(0.02835, res5.get().doubleValue(), 0.001);
+	}
+
+	
+	@Test
+	public void test5() {
+		Optional<BigDecimal> res6 = service.findConversionValue("CM", "KG");
+		assertEquals(Optional.empty(), res6);
+	}
+
+	@Test
+	public void test6() {
+		Optional<BigDecimal> res7 = service.findConversionValue("CM", "FT");
+		assertEquals(0.0328084, res7.get().doubleValue(), 0.001);
+	}
 }
