a0d495af60073b77ce50b8544d6038ca9c500430
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-10-04 16:32:59 +0700

    Make bundled app for PurchaseOrder run

--- src/main/front/src/application/purchase/PurchaseOrderApp.js
+++ src/main/front/src/application/purchase/PurchaseOrderApp.js
@@ -2,4 +2,15 @@ import Vue from 'vue';
 import PurchaseOrderPage from './PurchaseOrderPage';
 import BasicWireframe from 'baseComponents/wireframes/BasicWireframe';
 
+var PurchaseOrderApp = {
+	props: ['defaultItem'],
+	components: { PurchaseOrderPage, BasicWireframe},
+	template: `
+		<BasicWireframe>
+			<h4>Purchase Order</h4>
+			<PurchaseOrderPage :default-item="defaultItem" />
+		</BasicWireframe>
+	`
+}
 
+export default PurchaseOrderApp;
--- src/main/front/webpack.config.js
+++ src/main/front/webpack.config.js
@@ -3,13 +3,25 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-	  purchaseOrder: './src/application/purchase/PurchaseOrderApp.js'
+	  purchaseOrder: './src/application/purchase/app.js',
+	  salesOrder: './src/application/sales/SalesOrderApp.js',
   },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
 	  modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+	  alias: {
+		  'vue': 'vue/dist/vue.esm.js'
+	  }
+  },
+  module: {
+	  rules: [
+		  {
+			  test: /\.css$/,
+			  use: ['style-loader', 'css-loader']
+		  }
+	  ]
   }
 };
