926b9d656024668cfa6892b7b1d5d981b5a605d8
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-10-04 17:10:58 +0700

    Move css import from page to app

--- src/main/front/src/application/purchase/app.js
+++ src/main/front/src/application/purchase/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue'
 import App from './PurchaseOrderApp.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
 
