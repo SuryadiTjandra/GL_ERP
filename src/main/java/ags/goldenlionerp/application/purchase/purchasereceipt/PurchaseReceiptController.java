52c568aa2c923f1c3dd157d627cd58a80cfa431e
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-10-12 16:24:17 +0700

    Make the tests pass by adding a resource processor and a new controller method

--- src/main/java/ags/goldenlionerp/application/purchase/purchasereceipt/PurchaseReceiptController.java
+++ src/main/java/ags/goldenlionerp/application/purchase/purchasereceipt/PurchaseReceiptController.java
@@ -2,12 +2,21 @@ package ags.goldenlionerp.application.purchase.purchasereceipt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RepositoryRestController
 public class PurchaseReceiptController {
 
 	@Autowired
 	private PurchaseReceiptRepository repo;
+	
+	@DeleteMapping("/purchaseReceipts/{id}")
+	public ResponseEntity<?> deleteNotAllowed(){
+		return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+	}
+	
 	
 	
 }
