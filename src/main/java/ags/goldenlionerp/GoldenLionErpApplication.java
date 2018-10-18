2b70ea35fadb8134801d4f625627ab29d48300ed
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-10-18 14:18:43 +0700

    Make bean lazily initialized

--- src/main/java/ags/goldenlionerp/GoldenLionErpApplication.java
+++ src/main/java/ags/goldenlionerp/GoldenLionErpApplication.java
@@ -5,11 +5,13 @@ import java.util.Optional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
+@Lazy
 @SpringBootApplication
 @EnableHypermediaSupport(type = { HypermediaType.HAL })
 @EnableJpaAuditing
