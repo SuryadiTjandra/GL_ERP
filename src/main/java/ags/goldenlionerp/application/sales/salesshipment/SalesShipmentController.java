673163142db1f777b7e8d4b38024c96332fcad84
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-11-14 15:22:56 +0700

    Add basic shipment creation function (incomplete)

--- src/main/java/ags/goldenlionerp/application/sales/salesshipment/SalesShipmentController.java
+++ src/main/java/ags/goldenlionerp/application/sales/salesshipment/SalesShipmentController.java
@@ -1,5 +1,6 @@
 package ags.goldenlionerp.application.sales.salesshipment;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,14 @@ import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,7 +26,10 @@ import com.google.common.collect.Lists;
 public class SalesShipmentController {
 
 	@Autowired private SalesShipmentRepository repo;
+	@Autowired private SalesShipmentService serv;
 	@Autowired private SalesShipmentIdConverter conv;
+	
+	private SalesShipmentPredicates pred = SalesShipmentPredicates.getInstance();
 	
 	@GetMapping("/{id}") @ResponseBody
 	public Resource<SalesShipmentHeader> getSalesShipment(@PathVariable("id") String id, 
@@ -32,5 +42,28 @@ public class SalesShipmentController {
 		SalesShipmentHeader header = new SalesShipmentHeader(receipts);
 		
 		return new SalesShipmentHeaderResource(header, assembler);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> post(@RequestBody SalesShipmentHeader shipmentRequest, PersistentEntityResourceAssembler assembler){
+		if (repo.exists(pred.sameHeaderAs(shipmentRequest.getDetails().get(0))))
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		
+		SalesShipmentHeader shipment = serv.createSalesShipments(shipmentRequest);
+		SalesShipmentHeaderResource shipmentResource = new SalesShipmentHeaderResource(shipment, assembler);
+		URI resUri = URI.create(shipmentResource.getId().getHref());
+		
+		return ResponseEntity.created(resUri).body(shipmentResource);
+		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> put(){
+		return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(){
+		return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
 	}
 }
---
+++ src/main/java/ags/goldenlionerp/application/sales/salesshipment/SalesShipmentService.java
@@ -0,0 +1,11 @@
+package ags.goldenlionerp.application.sales.salesshipment;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalesShipmentService {
+
+	public SalesShipmentHeader createSalesShipments(SalesShipmentHeader shipmentRequest) {
+		return shipmentRequest;
+	}
+}
