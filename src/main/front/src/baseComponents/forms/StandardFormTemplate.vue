7eeeb09fa06092b05c17eec5f1d6fff9d0c33e38
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-11-30 17:52:43 +0700

    Extract form into mixin and template

---
+++ src/main/front/src/baseComponents/forms/StandardFormTemplate.vue
@@ -0,0 +1,21 @@
+<template>
+	<b-form @submit.prevent.stop="emit('submit', $event)" novalidate :validated="validated">
+    <slot></slot>
+    <b-form-row class="mt-2 mb-4">
+      <b-button variant="secondary" @click="$emit('cancel')" class="mx-2">Kembali</b-button>
+      <b-button type="submit" variant="primary"  v-if="submittable" class="mx-2">Simpan</b-button>
+    </b-form-row>
+  </b-form>
+</template>
+
+<script>
+export default {
+  props: {
+    submittable: Boolean,
+    validated: {
+      type:Boolean,
+      default: false
+    }
+  }
+}
+</script>
