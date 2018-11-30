ccdf541e82abb76b352b0e199c39d979716af169
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2018-11-30 14:48:07 +0700

    Extract component and mixin from common page formats

---
+++ src/main/front/src/baseComponents/pages/TableAndFormPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-container fluid>
+    <transition name="fade">
+      <div v-show="!formVisible">
+        <slot name="table"></slot>
+      </div>
+    </transition>
+    <transition name="fade">
+      <b-container fluid v-show="formVisible">
+        <slot name="form"></slot>
+      </b-container>
+    </transition>
+  </b-container>
+</template>
+
+<script>
+export default {
+  props: {
+    formVisible: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
---
+++ src/main/front/src/baseComponents/pages/TableAndFormPageMixin.js
@@ -0,0 +1,92 @@
+export default {
+  props: {
+    apiUrl: {
+      type: String,
+      required: true
+    },
+    defaultItem: {
+      type:Object,
+      default: {}
+    }
+  },
+  data: function(){
+    return {
+      formItem: {},
+      formMode: "add",
+      formVisible: false
+    }
+  },
+  methods: {
+    onCreateItem: function(event){
+      this.formItem = this.defaultItem;
+      this.formMode = "add";
+      this.formVisible = true;
+    },
+    onViewItem: function(item){
+      this.formItem = item;
+      this.formMode = "view";
+      this.formVisible = true;
+    },
+    onEditItem: function(item){
+      this.formItem = item;
+      this.formMode = "edit";
+      this.formVisible = true;
+    },
+    onFormCancel: function(){
+      this.formVisible = false;
+      this.formItem = {};
+    },
+    onFormSave: function(formItem){
+
+      const method = this.formMode == "add" ? "POST" : "PATCH";
+      const link = this.formMode == "add" ? this.apiUrl : formItem._links.self.href;
+
+      const csrfHeader = document.getElementsByName("_csrf_header")[0].getAttribute("content");
+      const csrfToken = document.getElementsByName("_csrf")[0].getAttribute("content");
+      let headers = new Headers();
+      headers.append(csrfHeader, csrfToken);
+      headers.append('Content-Type', 'application/json');
+
+      fetch(link, {
+        method: method,
+        body: JSON.stringify(formItem),
+        headers: headers
+      })
+      .then(res => res.ok ?
+            res.json() :
+            Promise.reject("Error: " + res.status + " " + res.statusText))
+      .then(res => {
+        alert("Sales Order created");
+        this.formVisible = false;
+        this.formItem = {};
+      })
+      .catch(err => alert(err));
+    }
+  },
+  computed: {
+    standardTableProps(){
+      return {
+        apiUrl: this.apiUrl
+      }
+    },
+    standardTableListeners(){
+      return {
+        'create-clicked': this.onCreateItem,
+        'edit-clicked': this.onEditItem,
+        'view-clicked': this.onViewItem
+      }
+    },
+    standardFormProps(){
+      return {
+        mode: this.formMode,
+        item: this.formItem
+      }
+    },
+    standardFormListeners(){
+      return {
+        'cancel': this.onFormCancel,
+        'save': this.onFormSave
+      }
+    }
+  }
+}
